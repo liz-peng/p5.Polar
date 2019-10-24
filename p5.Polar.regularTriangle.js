41ade4fe58d80920f6aa0424dda71e4593021b91
lizpeng.tw <lizpeng.tw@gmail.com>
2019-10-23 22:18:17 -0400

    [js] update regular triangle's function

--- p5.Polar.regularTriangle.js
+++ p5.Polar.regularTriangle.js
@@ -1,10 +1,10 @@
 Polar.regularTriangle = function(_angle, _radius, _distance) {
-  _angle2 = radians(_angle);
-  translate(sin(_angle2)*_distance, cos(_angle2)*-_distance);
-  rotate(radians(_angle))
+  _radians = radians(_angle);
+  translate(sin(_radians)*_distance, cos(_radians)*-_distance);
+  rotate(radians(_angle));
   triangle(
-    sin(0),cos(0)*-_radius,
-    sin(TWO_PI*1/3)*_radius,cos(TWO_PI*1/3)*-_radius,
-    sin(TWO_PI*2/3)*_radius,cos(TWO_PI*2/3)*-_radius
+    sin(0), cos(0)*-_radius,
+    sin(TWO_PI*1/3)*_radius, cos(TWO_PI*1/3)*-_radius,
+    sin(TWO_PI*2/3)*_radius, cos(TWO_PI*2/3)*-_radius
   );
 }

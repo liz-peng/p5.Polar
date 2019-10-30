fb30709981b99d6c300a50772d5a3577c27113b9
lizpeng.tw <lizpeng.tw@gmail.com>
2019-10-29 23:16:18 -0400

    Update triangle function

--- Polar.triangle.js
+++ Polar.triangle.js
@@ -1,7 +1,10 @@
-Polar.regularTriangle = function(_angle, _radius, _distance) {
-  let _radians = radians(_angle);
+
+Polar.triangle = function(_angle, _radius, _distance) {
+  resetMatrix();
+  translate(Polar.center.x, Polar.center.y);
+  const _radians = radians(_angle);
   translate(sin(_radians)*_distance, cos(_radians)*-_distance);
-  rotate(radians(_angle));
+  rotate(radians(_angle)); 
   triangle(
     sin(0), cos(0)*-_radius,
     sin(TWO_PI*1/3)*_radius, cos(TWO_PI*1/3)*-_radius,

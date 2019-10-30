edd75fdacccb960369b869ca39eccccaf569afa5
lizpeng.tw <lizpeng.tw@gmail.com>
2019-10-29 23:53:28 -0400

    Add center point x, y to line function and add multiple lines function

---
+++ Polar.line.js
@@ -0,0 +1,26 @@
+Polar = {
+  center: { 
+    x: 0, 
+    y: 0 
+  },
+  setCenter(x, y) {
+    this.center.x = x;
+    this.center.y = y;
+  }
+};
+
+Polar.line = function(_angle, _radius) {
+  resetMatrix();
+  translate(Polar.center.x, Polar.center.y);
+  const _radians = radians(_angle);
+  rotate(radians(_angle));
+  line(0, _radius, 0, -_radius);
+}
+
+
+Polar.lines = function(_num, _radius) {
+  const _angle = 360/_num;
+  for( let i=0; i<_num; i++) {
+    Polar.line(i*_angle, _radius)
+  }
+}
--- p5.Polar.line.js
+++
@@ -1,5 +0,0 @@
-Polar.line = function(_angle, _radius) {
-  let _radians = radians(_angle);
-  rotate(radians(_angle));
-  line(0, _radius, 0, -_radius);
-}

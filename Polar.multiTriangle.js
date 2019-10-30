a8f4ded22ae62d257b642f74094564ecf79dd93f
lizpeng.tw <lizpeng.tw@gmail.com>
2019-10-29 23:21:59 -0400

    Add center point x, y

--- Polar.multiTriangle.js
+++ Polar.multiTriangle.js
@@ -1,3 +1,14 @@
+Polar = {
+  center: { 
+    x: 0, 
+    y: 0 
+  },
+  setCenter(x, y) {
+    this.center.x = x;
+    this.center.y = y;
+  }
+};
+
 Polar.multiTriangle = function(_num, _radius, _distance) {
   const _angle = 360/_num;
   for(let i=0; i<_num; i++) {

8751e14723b6ea2b5b2789efca0c9ffa95a29225
lizpeng.tw <lizpeng.tw@gmail.com>
2019-10-30 00:02:38 -0400

    Add center point x, y to hexagon function and add multiple hexagons function

---
+++ Polar.hexagon.js
@@ -0,0 +1,30 @@
+Polar = {
+  center: { 
+    x: 0, 
+    y: 0 
+  },
+  setCenter(x, y) {
+    this.center.x = x;
+    this.center.y = y;
+  }
+};
+
+Polar.hexagon = function(_angle, _radius, _distance) {
+  resetMatrix();
+  translate(Polar.center.x, Polar.center.y);
+  const _radians = radians(_angle);
+  translate(sin(_radians)*_distance, cos(_radians)*-_distance);
+  rotate(radians(_angle));
+  beginShape();
+  for(let i=1; i<=6; i++) {
+    vertex(cos(TWO_PI*i/6)*_radius, sin(TWO_PI*i/6)*_radius);
+  }
+  endShape(CLOSE);
+}
+
+Polar.hexagons = function(_num, _radius, _distance) {
+  const _angle = 180/_num;
+  for(let i=0; i<_num; i++) {
+    Polar.hexagon(i*_angle, _radius, _distance);
+  }
+}

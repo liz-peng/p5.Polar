dc8161cf17f4bdcf121e9716ae41c7633ebe6c59
lizpeng.tw <lizpeng.tw@gmail.com>
2019-10-30 21:31:14 -0400

    Update hexagon function

--- Polar.hexagon.js
+++ Polar.hexagon.js
@@ -16,7 +16,7 @@ Polar.hexagon = function(_angle, _radius, _distance) {
   translate(sin(_radians)*_distance, cos(_radians)*-_distance);
   rotate(radians(_angle));
   beginShape();
-  for(let i=1; i<=6; i++) {
+  for(let i=0; i<6; i++) {
     vertex(cos(TWO_PI*i/6)*_radius, sin(TWO_PI*i/6)*_radius);
   }
   endShape(CLOSE);

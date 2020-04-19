7e5b5a234111b85587a831ba2299bf3db49f8e90
lizpeng.tw <lizpeng.tw@gmail.com>
2020-04-19 12:13:35 -0500

    [ellipse] make ellipse function support width and height radius seperately

--- polarEllipse.js
+++ polarEllipse.js
@@ -9,21 +9,21 @@ const polar = {
   }
 };
 
-p5.prototype.polarEllipse = function(_angle, _radius, _distance) {
+p5.prototype.polarEllipse = function(_angle, _radiusW, _radiusH, _distance) {
   resetMatrix();
   translate(polar.center.x, polar.center.y);
   const _radians = radians(_angle);
   translate(sin(_radians)*_distance, cos(_radians)*_distance);
-  ellipse(0, 0, _radius*2);
+  ellipse(0, 0, _radiusW*2, _radiusH*2);
 }
 
-p5.prototype.polarEllipses = function(_num, _radius, _distance, callback) {
+p5.prototype.polarEllipses = function(_num, _radiusW, _radiusH, _distance, callback) {
   const _angle = 360/_num;
   for(let i=1; i<=_num; i++) {
     if(callback) {
-      const _result = callback(i, _angle, _radius, _distance);
-      polarEllipse(_result[0]*_result[1], _result[2], _result[3]);
+      const _result = callback(i, _angle, _radiusW, _radiusH, _distance);
+      polarEllipse(_result[0]*_result[1], _result[2], _result[3], _result[4]);
     }
-    else polarEllipse(i*_angle, _radius, _distance);
+    else polarEllipse(i*_angle, _radiusW, _radiusH, _distance);
   }
 }
